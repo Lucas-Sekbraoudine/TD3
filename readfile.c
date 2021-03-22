987d7f9188f7024117ad658d0c1ebff537e19cf1
Lucas-Sekbraoudine <grp12456@gmail.com>
2021-03-22 15:31:05 +0100

    début exo 1

--- readfile.c
+++ readfile.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+
 
 /**
  * Ouvre le fichier message.txt du répertoire courant et affiche son contenu à l'écran.
  */
 int main() {
+    char *b
+    int fd,sz;
+    fd = open("message.txt", O_RDONLY);
 
 }
